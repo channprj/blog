729e9427bb60d1d2998b830fea2ed1cfc1d2ca95
CHANN <chann@chann.kr>
2019-05-20 17:28:29 +0900

    Update: Change code highlight - One Dark

--- config.js
+++ config.js
@@ -17,21 +17,21 @@ module.exports = {
       label: 'About',
       path: '/about'
     },
-    {
-      label: 'Contact',
-      path: '/contact'
-    }
+    // {
+    //   label: 'Contact',
+    //   path: '/contact'
+    // }
   ],
   author: {
     name: 'CHANN',
     photo: '/profile.png',
-    bio: '공사중입니다.',
-    // bio: '작성된 글의 의견과 현재 저의 의견은 다를 수 있습니다. 더 나은 점이나 잘못된 점에 대해 말씀을 해주신다면 감사히 받아들이고 배우겠습니다.',
+    // bio: '공사중입니다.',
+    bio: '작성된 글의 의견과 현재 저의 의견은 다를 수 있습니다. 더 나은 점이나 잘못된 점에 대해 말씀을 해주신다면 감사히 받아들이고 배우겠습니다.',
     contacts: {
-      email: 'chann@chann.kr',
-      telegram: 'channprj',
-      twitter: 'channprj',
       github: 'channprj',
+      twitter: 'channprj',
+      telegram: 'channprj',
+      email: 'chann@chann.kr',
       rss: 'rss.xml',
     }
   }
